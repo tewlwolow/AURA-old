8018854365b176c38ab3d686343131ace5d9ad30
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2022-08-30 21:41:34 +0200

    Super hacky forced encoding stunt

--- i18n/init.lua
+++ i18n/init.lua
@@ -7,18 +7,22 @@ local defaultLanguage = require("tew.AURA.i18n.en")
 local config = require("tew.AURA.config")
 local language = require(config.language)
 
-for i, cat in pairs(defaultLanguage) do
-	if not language[i] then
-		language[i] = defaultLanguage[i]
-	else
-		for ii, _ in pairs(cat) do
-			if (not language[i][ii])
-				or (language[i][ii] == "")
-				or (language[i][ii] == {}) then
-				language[i][ii] = defaultLanguage[i][ii]
-			end
+table.copymissing(language, defaultLanguage)
+
+-- This is some super silly stuff nicked from official MWSE i18n, but gets the job done, at least for FR
+local function convertUTF8Table(t, lang)
+	for k, v in pairs(t) do
+		local vType = type(v)
+		if (vType == "string") then
+			--- @diagnostic disable-next-line:undefined-field
+			t[k] = mwse.iconv(lang, v)
+		elseif (vType == "table") then
+			convertUTF8Table(v, lang)
 		end
 	end
 end
+
+convertUTF8Table(language, "eng")
+
 
 mwse.log("[AURA i18n]: Translation verified. Missing values have been filled in with defaults.")
